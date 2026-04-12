359346cb05d67758e7cd3013dd7edfb699c03117
egsa2009 <egsa2005@gmail.com>
2026-04-12 17:03:26 -0500

    Fix colores sidebar y logout

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,13 @@ export default {
   theme: {
     extend: {
       colors: {
-        primary:      '#2A9D8F',
-        'primary-dark': '#208a7d',
-        dark:         '#0A1F44',
-        accent:       '#BFA16F',
+        primary:          '#00b8cc',
+        'primary-dark':   '#008fa0',
+        'primary-darker': '#006d7a',
+        dark:             '#0A1F44',
+        'dark-light':     '#132a5c',
+        accent:           '#FF6B00',
+        'accent-dark':    '#cc5500',
       }
     }
   },
